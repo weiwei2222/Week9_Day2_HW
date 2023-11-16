4a77f451d2e9f96a61a72e3a2d6d16320a7c85b7
Weiwei <linweiwei2222@163.com>
2023-11-15 19:35:55 -0500

    homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Results from "./components/Results";
 
 function App() {
-  const [userInput, setUserInput] = useState(0);
+  const [userInput, setUserInput] = useState("0");
 
   const handleChange = (event) => {
     setUserInput(event.target.value);
   };
 
-  function checkCard(event) {
-    event.preventDefault();
+  function checkCard() {
+    if (userInput.length < 1) return;
     const userNum = userInput.split("");
     for (let i = userNum.length - 2; i >= 0; i -= 2) {
       userNum[i] = userNum[i] * 2;
     }
-    console.log(userNum);
 
     // use Regular expressions: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions
     // \D find a number, Matches any character that is not a digit (Arabic numeral).
@@ -23,28 +22,27 @@ function App() {
     // /g Global search
     // ["2","6","4",18,"4"] will be ["2","4","1","8","4"]
     let number = (userNum + "").replace(/\D+/g, "").split("");
-    console.log(number);
 
     let numArray = number.map((num) => Number(num));
-    console.log(numArray);
 
     const totalNum = numArray.reduce((acc, current) => acc + current, 0);
-    console.log(totalNum);
 
     if (totalNum % 10 == 0) {
-      return console.log(true);
+      return true;
     } else {
-      return console.log(false);
+      return false;
     }
   }
+  useEffect(() => {
+    checkCard(userInput);
+  });
 
   return (
     <>
       <h1>Please enters a credit card </h1>
       <form>
         <input type="number" onChange={handleChange} value={userInput} />
-        <button onClick={checkCard}>check card</button>
-        <Results />
+        <Results result={checkCard(userInput)} />
       </form>
     </>
   );
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Results() {
-  return <div>Results</div>;
+function Results({ result }) {
+  return (
+    <h3 style={{ color: "red" }}>
+      {result ? "The card is valid." : "The card is NOT valid."}
+    </h3>
+  );
 }
 
 export default Results;
